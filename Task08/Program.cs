9ef61f58ba6d50093916eb4d415135c3bc7032d5
Tra-Ta <madtazik@gmail.com>
2022-11-17 23:09:54 +0300

    Complete_Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter a natural number -> ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int n = 0;
+int n1 = 0;
+if (number > 0 )
+{
+
+while (n < number)
+    {
+        n += 1;
+        n1 += 1;
+        {
+            int result = n1 % 2;
+            if (result == 0)
+            {
+            
+            Console.Write($"{n} ");
+            }
+            
+        }
+        
+    }
+}
+else Console.WriteLine("Not a natural number");
