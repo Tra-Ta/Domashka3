2f90916d340d2c639e7d5743d8474b1cf8c529f3
Tra-Ta <madtazik@gmail.com>
2022-11-17 20:11:14 +0300

    Complete_Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите два числа ->");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"{number1} больше {number2}");
+}
+else Console.WriteLine($"{number2} больше {number1}");
