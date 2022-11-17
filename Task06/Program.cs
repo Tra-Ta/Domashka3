97cdb73fd82440d58b507b36d34fb1b0437d5daf
Tra-Ta <madtazik@gmail.com>
2022-11-17 21:41:00 +0300

    Complete_Task04,Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a number -> ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number % 2;
+
+if (result == 0) 
+{
+    Console.WriteLine("The number is even");
+}
+else Console.WriteLine("The number is odd");
